84e3648af710da85dae822cce6352e660f2371ef
noortiel <noortje.goedhart@outlook.com>
2025-03-07 16:51:00 +0100

    Triggering the timeline works now!

---
+++ BPWREAL/Assets/Scripts/CutsceneTimeline.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class CutsceneTimeline : MonoBehaviour
+{
+    [SerializeField] PlayableDirector director;
+    
+
+    public void StartCutsceneTimeline()
+    {
+        director.Play();
+
+        Debug.Log("Timeline!");
+    }
+}
